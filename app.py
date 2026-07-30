d5255196cb9ed262ed961d71c8f548dd2a46ed78
Ashish Jaiswal <67005570+Ashishrxl@users.noreply.github.com>
2026-07-30 17:55:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ if st.button("Search / Generate", type="primary"):
         # Generator function to stream tokens directly into Streamlit
         def stream_response():
             response_stream = client.models.generate_content_stream(
-                model="gemini-2.5-flash-lite",
+                model="gemini-2.0-flash",
                 contents=contents,
                 config=generate_content_config,
             )
